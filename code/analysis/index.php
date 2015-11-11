26caddc4f0dafeefea09551304f50de342ff0dd0
dboth <github@dboth.de>
2015-11-12 00:16:02 +0100

    added comments

--- code/analysis/index.php
+++ code/analysis/index.php
@@ -1,25 +1,43 @@
 <?php
+//include the configs
 require_once __DIR__."/../inc/config.php";
+
+//set the output to json
 header('Content-Type: application/json');
+
+//check if there was data sent
 if (empty($_POST["data"]))
     die("no data"); //todo handle error -> no data, should return a json error object
+
+//try to decode the data. data must be in json
 $data = json_decode($_POST["data"], true);
+
+//check if data is json
 if ($data === null)
     die("no json"); //todo handle error -> unreadable json, should return a json error object
+
+//create a new data object with meta data
 $new_json = array(
     "data"=>$data, 
-    "session"=>array(
+    "meta"=>array(
         //todo: add serverside information
     )
 );
-if (DIRECTORY_SEPARATOR == '\\' && $GLOBALS["conf"]["testing_on_windows"]){
+
+//escapeshellarg is shitty for windows because it replaces all double quotes.
+//therefore if on windows and testing do not use that and escape the argument with a rudimentary version of the escaper
+if (DIRECTORY_SEPARATOR == '\\' && $GLOBALS["conf"]["testing"]){
+    //the rudimentary escaper
     function shitty_escapeshellarg($st){
         return '"'.str_replace('"','\\"',$st).'"';
     }
-$argument = shitty_escapeshellarg(json_encode($new_json));
+    $argument = shitty_escapeshellarg(json_encode($new_json));
 } else {
-$argument = escapeshellarg(json_encode($new_json));    
+    //on linux everythings fine
+    $argument = escapeshellarg(json_encode($new_json));    
 }
+//create the command
 $command = __DIR__."/tgd.py $argument";
 
+//execute the command (system() prints the output)
 system($command); 

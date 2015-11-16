bfd1e8d4686893c0b747c10ce46efbc7930ae90c
dboth <github@dboth.de>
2015-11-16 15:03:13 +0100

    debug

--- code/js/create_analysis.js
+++ code/js/create_analysis.js
@@ -4,7 +4,10 @@ $(document).ready(function () {
         console.log("Entered Click");
         
         //var field_data = $("#dictation-text").serialize();
-        var field_data = 'data=' + "\"{ 'data' : '" + $("#dictation-text").val() + "' }\"";
+        //var field_data = 'data=' + "\"{ 'data' : '" + $("#dictation-text").val() + "' }\"";
+        var field_data = {data: $("#dictation-text").val(),
+                            was_anderes: "Test"
+                            };
         console.log(field_data);
         console.log(typeof field_data);
         var action = $(this).attr("action");
@@ -13,7 +16,7 @@ $(document).ready(function () {
         
         $.ajax({
             url: action,
-            data: field_data,
+            data: JSON.stringify(field_data),
             type: method,
             error: function (a,b,c){console.log(a,b,c);},
             complete: function (res){console.log(res);}

8612d0bbdbae74d67e0c9195e5bc29ae4d7e6d1e
weidler <web@tonioweidler.de>
2015-11-16 15:13:33 +0100

    cleaned

--- code/js/create_analysis.js
+++ code/js/create_analysis.js
@@ -2,14 +2,12 @@ $(document).ready(function () {
     $("#dictation-form").submit(function (event) {
         event.preventDefault();
         console.log("Entered Click");
+
+        var field_data = {
+            text: $("#dictation-text").val(),
+            was_anderes: "Test"
+        };
         
-        //var field_data = $("#dictation-text").serialize();
-        //var field_data = 'data=' + "\"{ 'data' : '" + $("#dictation-text").val() + "' }\"";
-        var field_data = {text: $("#dictation-text").val(),
-                            was_anderes: "Test"
-                            };
-        console.log(field_data);
-        console.log(typeof field_data);
         var action = $(this).attr("action");
         var method = $(this).attr("method");
         console.log(action);
@@ -20,6 +18,6 @@ $(document).ready(function () {
             type: method,
             error: function (a,b,c){console.log(a,b,c);},
             complete: function (res){console.log(res);}
-            });
+        });
     });
 });

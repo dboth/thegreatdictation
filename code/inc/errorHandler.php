2b249112114d4d82daebc046aa007150e50d8dd9
weidler <web@tonioweidler.de>
2015-11-27 14:45:14 +0100

    nothing

---
+++ code/inc/errorHandler.php
@@ -0,0 +1,20 @@
+<?php
+
+class ErrorHandler {
+     protected $error_code;
+
+     public function __construct($error_code) {
+         $this->error_code = $error_code;
+     }
+
+     public function writeLogMessage() {
+
+     }
+
+     public function sendUserMessage() {
+
+     }
+
+}
+
+?>

ebe186ecaf1115573ad2f78b658aaf56ff148e50
dboth <github@dboth.de>
2015-12-16 11:51:39 +0100

    fed

--- sockets/feedback.php
+++ sockets/feedback.php
@@ -1,9 +1,24 @@
 <?php
+
+//TAKES POST REQUEST WITH PARAMS:
+//title, subject, text
+//AND SAVES THEM IN THE DB
+
+
+
 require_once __DIR__."/../inc/config.php";
 require_once __DIR__."/../inc/sqlConnector.php";
 
+$title = $_POST["title"];
+$subject = $_POST["subject"];
+$text = $_POST["text"];
+
+if (empty($title) || empty($subject) || empty($text)) 
+    die("ERROR");
+
 $db = new SqlConnector();
-if ($db->putFeedback($title, $subject, $text))
-    echo "OK";
-else 
-    echo "ERROR";
+
+if (!$db->putFeedback($title, $subject, $text))
+    die("ERROR");
+
+die("OK");

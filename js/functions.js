e8f2fb68d2aa21aabd0667fee7c0e61fb373a0db
weidler <web@tonioweidler.de>
2016-01-16 11:42:40 +0100

    Styling and Scaffold for AlignmentPostProcessor

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,21 @@
 function convertStringToHTML(str) {
     return str.replace(/\n/g, "<br>");
-};
+}
+
+function toggleViews(to_view) {
+
+    /*
+    * Function to control the toggling between dictation form and analysis
+    */
+
+    if (!$(to_view).hasClass('main-container')) {
+        console.log($(to_view).attr("id"));
+        $(".main-container").fadeOut(0, function () {
+            $(this).removeClass("main-container");
+            $(to_view).fadeIn(0, function () {
+                $(this).addClass("main-container");
+            });
+        });
+    }
+
+}

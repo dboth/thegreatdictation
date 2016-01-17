dc2792cec288ce9784813e580fcee89a8786b4c6
dboth <github@dboth.de>
2016-01-17 20:56:12 +0100

    f

---
+++ documents/database1.sql
@@ -0,0 +1,143 @@
+-- --------------------------------------------------------
+-- Host:                         localhost
+-- Server Version:               5.5.46-0+deb7u1 - (Debian)
+-- Server Betriebssystem:        debian-linux-gnu
+-- HeidiSQL Version:             9.3.0.4984
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Exportiere Struktur von Tabelle softpro_ws15_1.audio
+DROP TABLE IF EXISTS `audio`;
+CREATE TABLE IF NOT EXISTS `audio` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `text_id` int(11) DEFAULT NULL,
+  `file` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `text_id` (`text_id`),
+  CONSTRAINT `audio_ibfk_1` FOREIGN KEY (`text_id`) REFERENCES `texts` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+
+-- Exportiere Struktur von Tabelle softpro_ws15_1.feedback
+DROP TABLE IF EXISTS `feedback`;
+CREATE TABLE IF NOT EXISTS `feedback` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` text,
+  `subject` text,
+  `text` text,
+  `time_sent` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+
+-- Exportiere Struktur von Tabelle softpro_ws15_1.results
+DROP TABLE IF EXISTS `results`;
+CREATE TABLE IF NOT EXISTS `results` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `text_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `meta` mediumtext,
+  PRIMARY KEY (`id`),
+  KEY `text_id` (`text_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `results_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `results_ibfk_1` FOREIGN KEY (`text_id`) REFERENCES `texts` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+
+-- Exportiere Struktur von Tabelle softpro_ws15_1.results_v0
+DROP TABLE IF EXISTS `results_v0`;
+CREATE TABLE IF NOT EXISTS `results_v0` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `input` text COLLATE utf8_bin,
+  `output` text COLLATE utf8_bin,
+  `time_inserted` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+
+-- Exportiere Struktur von Tabelle softpro_ws15_1.sentences
+DROP TABLE IF EXISTS `sentences`;
+CREATE TABLE IF NOT EXISTS `sentences` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `text_id` int(11) DEFAULT NULL,
+  `content` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `text_id` (`text_id`),
+  CONSTRAINT `sentences_ibfk_1` FOREIGN KEY (`text_id`) REFERENCES `texts` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+
+-- Exportiere Struktur von Tabelle softpro_ws15_1.sentence_results
+DROP TABLE IF EXISTS `sentence_results`;
+CREATE TABLE IF NOT EXISTS `sentence_results` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `result_id` int(11) DEFAULT NULL,
+  `sentence_id` int(11) DEFAULT NULL,
+  `input` mediumtext,
+  PRIMARY KEY (`id`),
+  KEY `result_id` (`result_id`),
+  KEY `sentence_id` (`sentence_id`),
+  CONSTRAINT `sentence_results_ibfk_2` FOREIGN KEY (`sentence_id`) REFERENCES `sentences` (`id`),
+  CONSTRAINT `sentence_results_ibfk_1` FOREIGN KEY (`result_id`) REFERENCES `results` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+
+-- Exportiere Struktur von Tabelle softpro_ws15_1.stops
+DROP TABLE IF EXISTS `stops`;
+CREATE TABLE IF NOT EXISTS `stops` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `audio_id` int(11) DEFAULT NULL,
+  `sentence_id` int(11) DEFAULT NULL,
+  `second` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `audio_id` (`audio_id`),
+  KEY `sentence_id` (`sentence_id`),
+  CONSTRAINT `stops_ibfk_2` FOREIGN KEY (`sentence_id`) REFERENCES `sentences` (`id`),
+  CONSTRAINT `stops_ibfk_1` FOREIGN KEY (`audio_id`) REFERENCES `audio` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+
+-- Exportiere Struktur von Tabelle softpro_ws15_1.texts
+DROP TABLE IF EXISTS `texts`;
+CREATE TABLE IF NOT EXISTS `texts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+
+-- Exportiere Struktur von Tabelle softpro_ws15_1.users
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Daten Export vom Benutzer nicht ausgewählt
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

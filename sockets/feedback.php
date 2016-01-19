baa2c3831a801fd7050c3af76d9d5824c17ae796
weidler <web@tonioweidler.de>
2016-01-19 23:55:37 +0100

    Feedback works now

--- sockets/feedback.php
+++ sockets/feedback.php
@@ -11,14 +11,14 @@ require_once __DIR__."/../inc/SqlConnector.php";
 
 $title = $_POST["title"];
 $subject = $_POST["subject"];
-$text = $_POST["text"];
+$text = $_POST["message"];
 
-if (empty($title) || empty($subject) || empty($message))
-    die("ERROR");
+if (empty($title) || empty($subject) || empty($text))
+    die($title." AND ".$subject." AND ".$text);
 
 $db = new SqlConnector();
 
-if (!$db->putFeedback($title, $subject, $text))
+if (!($db->saveFeedback($title, $subject, $text)))
     die("ERROR");
 
 die("OK");

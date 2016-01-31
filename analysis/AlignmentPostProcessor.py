a57e7b4fe9e6e159a899f9c3d0e3c44493d48443
svenja-l <svenja.now@googlemail.com>
2016-01-31 21:54:32 +0100

    calc Result
    wirft keinen error, muss aber noch verbessert werden

--- analysis/AlignmentPostProcessor.py
+++ analysis/AlignmentPostProcessor.py
@@ -5,17 +5,17 @@ from collections import namedtuple
 
 class AlignmentPostProcessor():
     def __init__(self, alignment, input_str, target_str, match):
-        
+
         #PATH
         self.alignment = alignment
-        
+
         #STRINGS
         self.input = input_str
         self.target = target_str
-        
+
         #BAGS
         self.output_dict = {}
-        
+
         #PROCESSED DATA
         self.matrix_field = namedtuple("Field", ["x", "y", "cost", "op"])
 
@@ -38,7 +38,7 @@ class AlignmentPostProcessor():
             target_iter = process[0]
             word_fault_sum += process[2][2]
             if self.target[target_iter-1] == " ":
-                if target_iter > cont_iter: #this assures that each whitespace only creates one word if several processes have the same input start value                    
+                if target_iter > cont_iter: #this assures that each whitespace only creates one word if several processes have the same input start value
                     self.output_dict[self.alignment[start_process_iter][2][0]] = [process[0]-2, self.target[self.alignment[start_process_iter][2][0]:process[0]-1], self.input[self.alignment[start_process_iter][2][1]:process[1]-1], self.alignment[start_process_iter][2][1], process[1]-2, word_fault_sum]
                     start_process_iter = process_iter+1
                     word_fault_sum = 0
@@ -51,6 +51,21 @@ class AlignmentPostProcessor():
                 cont_iter = target_iter
         self.output_dict[self.alignment[start_process_iter][2][0]] = [process[0]-1, self.target[self.alignment[start_process_iter][2][0]:self.alignment[::-1][0][0]+1], self.input[self.alignment[start_process_iter][2][1]:], self.alignment[start_process_iter][2][1], self.alignment[-1][1]-1, word_fault_sum]
         return self.output_dict
+
+    def calcResult(self):
+        error_weight = 0
+        all_errors = 0
+        wrong_words = 0
+        words = len(self.output_dict)
+        for key in self.output_dict:
+            error_weight = self.output_dict[key][-1]
+            if error_weight != 0:
+                all_errors += error_weight
+            else:
+                wrong_words += 1
+        score = all_errors/words
+        #print(score,wrong_words,words)
+        return score, wrong_words, words
 
 if __name__ == "__main__":
 	a = Aligner.Aligner(u"halo ichbin ein n kenguru", u"hallo ich bin ein känguruh")

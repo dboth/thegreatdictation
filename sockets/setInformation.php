4486f381c9a468dc7da8329c8b03f605bac80a63
dboth <github@dboth.de>
2016-02-21 19:44:12 +0100

    usersystem

--- sockets/setInformation.php
+++ sockets/setInformation.php
@@ -1,4 +1,27 @@
 <?php
+
+/*
+ 
+this is the interface for the usersystem to javascript
+
+takes a post request with the following parameters:
+ * username: string
+ * information: json object
+
+if username is not empty the username gets set and the information for this 
+username in the database will be written in the session key-value storage.
+All existing key-values in the session will be deleted.
+
+if information contains elements the key will if allowed saved with its value 
+into the key-value storage. If a username was set before the data will also be
+saved into the database.
+
+The request returns a json object resembling the current key-value storage. 
+Username and information can both be empty to only receive this without any
+changes.
+
+*/
+
 require_once "../inc/UserSystem.php";
 require_once "../inc/ErrorHandler.php";
 $usersystem = new UserSystem();

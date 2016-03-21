5db14c43563bbf03b2deb39b08c9ba57c9b93c49
svenja-l <svenja.now@googlemail.com>
2016-03-21 21:15:24 +0100

    audios

--- sql/insert_audios.sql
+++ sql/insert_audios.sql
@@ -1,1 +1,10 @@
-INSERT INTO `softpro_ws15_1`.`audio` (`id`, `text_id`, `file`) VALUES (NULL, '1', 'a1_3.wav');
+INSERT INTO `softpro_ws15_1`.`audio` (`id`, `text_id`, `file`) VALUES (NULL, '12', 'a1_1.wav');
+INSERT INTO `softpro_ws15_1`.`audio` (`id`, `text_id`, `file`) VALUES (NULL, '13', 'a1_2.wav');
+INSERT INTO `softpro_ws15_1`.`audio` (`id`, `text_id`, `file`) VALUES (NULL, '14', 'a1_3.wav');
+INSERT INTO `softpro_ws15_1`.`audio` (`id`, `text_id`, `file`) VALUES (NULL, '15', 'a2_1.wav');
+INSERT INTO `softpro_ws15_1`.`audio` (`id`, `text_id`, `file`) VALUES (NULL, '16', 'a2_2.wav');
+INSERT INTO `softpro_ws15_1`.`audio` (`id`, `text_id`, `file`) VALUES (NULL, '17', 'a2_3.wav');
+INSERT INTO `softpro_ws15_1`.`audio` (`id`, `text_id`, `file`) VALUES (NULL, '18', 'a2_4.wav');
+INSERT INTO `softpro_ws15_1`.`audio` (`id`, `text_id`, `file`) VALUES (NULL, '19', 'a2_5.wav');
+INSERT INTO `softpro_ws15_1`.`audio` (`id`, `text_id`, `file`) VALUES (NULL, '20', 'a2_6.wav');
+
